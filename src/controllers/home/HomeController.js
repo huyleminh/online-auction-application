46d6297f5194d657c07f9c5c1e43b5e7923cac6e
Huy Le Minh <leminhhuy.hcmus@gmail.com>
2022-01-05 11:37:40 +0700

    Add load top 5 products: most bidded, highest price, due date in home page

--- src/controllers/home/HomeController.js
+++ src/controllers/home/HomeController.js
@@ -1,4 +1,8 @@
+import moment from "moment";
+import ProductModel from "../../models/ProductModal.js";
 import AppController from "../AppController.js";
+import CommonConst from "../../shared/CommonConst.js";
+import numeral from "numeral";
 
 export default class HomeController extends AppController {
     constructor() {
@@ -9,19 +13,144 @@ export default class HomeController extends AppController {
     init() {
         this._router.get("/", this.renderHome);
         this._router.get("/about", this.renderAbout);
+
+        this._router.get("/api/home/top5due", this.getTop5DueProducts);
+        this._router.get("/api/home/top5highest", this.getTop5HighestProducts);
+        this._router.get("/api/home/top5bid", this.getTop5BidProducts);
     }
 
     renderHome(req, res) {
         res.render("pages/home", {
             products: {
-                topDue: [1, 2, 3, 4, 5, 6, 7],
-                topBidded: [1, 2, 3, 4, 5, 6, 7],
-                topHighest: [1, 2, 3, 4, 5, 6, 7],
+                initArray: [1, 2, 3, 4],
             },
         });
     }
 
     renderAbout(req, res) {
         res.render("pages/about");
+    }
+
+    async getTop5DueProducts(req, res) {
+        try {
+            const [top5DuePromise] = await ProductModel.getTop5Due();
+
+            let result;
+            if (top5DuePromise === undefined) {
+                result = [];
+            } else {
+                result = top5DuePromise.map((item) => {
+                    let createdDate = moment(item.created_date).locale("en").fromNow();
+                    const dayDiff = -1 * moment().diff(moment(item.expired_date), "days");
+                    const ret = {
+                        productId: item.product_id,
+                        productName: item.product_name,
+                        thumbnail: item.thumbnail,
+                        currentPrice: numeral(item.current_price).format("0,0"),
+                        // expiredDate: moment(item.expired_date).format(
+                        //     CommonConst.MOMENT_BASE_USER_FORMAT
+                        // ),
+                        totalBids: item.bid_count !== null ? item.bid_count : 0,
+                        createdDate,
+                        expiredDate: moment(item.expired_date).locale("en").from(),
+                        firstName: item.first_name,
+                        dayDiff,
+                    };
+
+                    if (item.buy_now_price !== null) {
+                        ret.buyNowPrice = numeral(item.buy_now_price).format("0,0");
+                    }
+
+                    return ret;
+                });
+            }
+
+            res.json({ status: 200, data: result });
+        } catch (error) {
+            console.log(error);
+            res.json({ status: 500 });
+        }
+    }
+
+    async getTop5HighestProducts(req, res) {
+        try {
+            const [top5HighestPromise] = await ProductModel.getTop5HighestPrice();
+
+            let result;
+            if (top5HighestPromise === undefined) {
+                result = [];
+            } else {
+                result = top5HighestPromise.map((item) => {
+                    let createdDate = moment(item.created_date).locale("en").fromNow();
+                    const dayDiff = -1 * moment().diff(moment(item.expired_date), "days");
+                    const ret = {
+                        productId: item.product_id,
+                        productName: item.product_name,
+                        thumbnail: item.thumbnail,
+                        currentPrice: numeral(item.current_price).format("0,0"),
+                        // expiredDate: moment(item.expired_date).format(
+                        //     CommonConst.MOMENT_BASE_USER_FORMAT
+                        // ),
+                        totalBids: item.bid_count !== null ? item.bid_count : 0,
+                        createdDate,
+                        expiredDate: moment(item.expired_date).locale("en").from(),
+                        firstName: item.first_name,
+                        dayDiff,
+                    };
+
+                    if (item.buy_now_price !== null) {
+                        ret.buyNowPrice = numeral(item.buy_now_price).format("0,0");
+                    }
+
+                    return ret;
+                });
+            }
+
+            res.json({ status: 200, data: result });
+        } catch (error) {
+            console.log(error);
+            res.json({ status: 500 });
+        }
+    }
+
+    async getTop5BidProducts(req, res) {
+        try {
+            const [top5BiddedPromise] = await ProductModel.getTop5Bid();
+
+            let result;
+            if (top5BiddedPromise === undefined) {
+                result = [];
+            } else {
+                result = top5BiddedPromise.map((item) => {
+                    let createdDate = moment(item.created_date).locale("en").fromNow();
+                    const dayDiff = -1 * moment().diff(moment(item.expired_date), "days");
+                    const ret = {
+                        productId: item.product_id,
+                        productName: item.product_name,
+                        thumbnail: item.thumbnail,
+                        currentPrice: numeral(item.current_price).format("0,0"),
+                        // expiredDate: moment(item.expired_date).format(
+                        //     CommonConst.MOMENT_BASE_USER_FORMAT
+                        // ),
+                        totalBids: item.bid_count !== null ? item.bid_count : 0,
+                        createdDate,
+                        expiredDate: moment(item.expired_date).locale("en").from(),
+                        firstName: item.first_name,
+                        dayDiff,
+                    };
+
+                    if (item.buy_now_price !== null) {
+                        ret.buyNowPrice = numeral(item.buy_now_price).format("0,0");
+                    }
+
+                    return ret;
+                });
+            }
+
+            res.json({ status: 200, data: result });
+        } catch (error) {
+            console.log(error);
+            res.json({ status: 500 });
+        }
     }
 }
--- src/utils/helpers/HbsHelper.js
+++ src/utils/helpers/HbsHelper.js
@@ -1,4 +1,5 @@
 import moment from "moment";
+import numeral from "numeral";
 import CommonConst from "../../shared/CommonConst.js";
 
 moment.locale("vi");
@@ -15,11 +16,16 @@ export default class HbsHelper {
         return moment(date).format(CommonConst.MOMENT_BASE_USER_FORMAT);
     }
 
+    static formatMoney(money) {
+        return numeral(val).format("0,0");
+    }
+
     static getAll() {
         return {
             isEqual: this.isEqual,
             isNotEqual: this.isNotEqual,
             formatDate: this.formatDate,
+            formatMoney: this.formatMoney,
         };
     }
 }
