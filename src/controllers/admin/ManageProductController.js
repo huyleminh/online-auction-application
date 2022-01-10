9bcb3cb07b1c5f97275c1d6c4014cfe77e925697
Nguyen Hua Hung <63572298+huahungnguyen121@users.noreply.github.com>
2022-01-10 16:58:19 +0700

    AUC-38 - Product management page (admin)

--- src/controllers/admin/ManageProductController.js
+++ src/controllers/admin/ManageProductController.js
@@ -1,3 +1,4 @@
+import AuthMiddlewares from "../../middlewares/AuthMiddlewares.js";
 import AppController from "../AppController.js";
 
 export default class ManageProductController extends AppController {
@@ -7,5 +8,27 @@ export default class ManageProductController extends AppController {
         this.init();
     }
 
-    init() {}
+    init() {
+        this._router.get(
+            '/admin/products',
+            AuthMiddlewares.authorizeAdmin,
+            this.renderProductManagement
+        );
+
+        this._router.post(
+            '/admin/products/delete',
+            AuthMiddlewares.authorizeAdmin,
+            this.deleteProduct
+        );
+    }
+
+    renderProductManagement(req, res) {
+        return res.render("pages/admin/product-list", {
+            layout: "admin",
+        })
+    }
+
+    deleteProduct(req, res) {
+        return;
+    }
 }
