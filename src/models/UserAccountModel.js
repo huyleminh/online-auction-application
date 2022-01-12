a26d044633b1a413459abdf872c60fa7cfd18a79
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2022-01-12 16:16:29 +0700

    AUC-52 - Implement API reset password
    - searchByColumnWithPagination: get list of users by a column or get all list of users if the caller has two last parameters. The first parameter is the current page.
    - getByUserId: get a specific user by the user_id field.

--- src/models/UserAccountModel.js
+++ src/models/UserAccountModel.js
@@ -1,4 +1,5 @@
 import KnexConnection from "../utils/KnexConnection.js";
+import CommonConst from "../shared/CommonConst.js";
 
 export default class UserAccountModel {
     static insert(data) {
@@ -127,5 +128,45 @@ export default class UserAccountModel {
                 reject(err);
             }
         });
+    }
+
+    static searchByColumnWithPagination(page, column, value) {
+        return new Promise(async function (resolve, reject) {
+            try {
+                const offset = (page - 1) * CommonConst.ITEMS_PER_TABLE_PAGE;
+                let whereStatement = ""
+                
+                if (column !== undefined && value !== undefined) {
+                    whereStatement = `where ${column} like '%${value}%'`
+                }
+
+                const resultSet = await KnexConnection.raw(
+                    `select * from user_account ${whereStatement} 
+                    limit ${CommonConst.ITEMS_PER_TABLE_PAGE + 1}
+                    offset ${offset}`
+                )
+                const hasNext = resultSet[0].length === CommonConst.ITEMS_PER_TABLE_PAGE + 1;
+                
+                resolve({
+                    hasNext: hasNext,
+                    data: resultSet[0].slice(0, CommonConst.ITEMS_PER_TABLE_PAGE),
+                });
+            } catch (error) {
+                reject(error);
+            }
+        })
+    }
+
+    static getByUserId(userId) {
+        return new Promise(async function (resolve, reject) {
+            try {
+                const resultSet = await KnexConnection('user_account')
+                    .where('user_id', userId);
+                
+                resolve(resultSet)
+            } catch (error) {
+                reject(error)
+            }
+        })
     }
 }
